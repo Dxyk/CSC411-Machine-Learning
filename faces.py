d3854aa505770a3e3b410af3b5d097e35256295a
Dxyk <danielkong1997@gmail.com>
2018-01-24 14:51:44 -0500

    init import for faces

---
+++ faces.py
@@ -0,0 +1,11 @@
+from pylab import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+import random
+import time
+from scipy.misc import *
+import matplotlib.image as mpimg
+import os
+from scipy.ndimage import filters
+import urllib
