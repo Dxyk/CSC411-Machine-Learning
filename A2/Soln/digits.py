49d10ddf18b66f99f5ef2c4cd7939b2c6f2af7b9
Dxyk <danielkong1997@gmail.com>
2018-02-19 17:34:19 -0500

    part 3

--- A2/Soln/digits.py
+++ A2/Soln/digits.py
@@ -16,17 +16,17 @@ def load_data():
 # Part 1
 def part1():
     """
-    Load the data and save 10 images each from 10 digits
+    Load the data and store 10 images for each digit
     """
     # Load the MNIST digit data
     data = load_data()
 
-    # loop over training numbers and pick 10 number each
-    for number in range(10):
-        for i in range(10):
-            im = data["train" + str(number)][i].reshape((28, 28)) / 255.0
-            file_name = "report/img/1/{}_{}.png".format(str(number), str(i))
-            imsave(file_name, im, cmap = cm.gray)
+    # # get first 10 image for each digit
+    # for number in range(10):
+    #     for i in range(10):
+    #         im = data["train" + str(number)][i].reshape((28, 28)) / 255.0
+    #         file_name = "report/img/1/{}_{}.png".format(str(number), str(i))
+    #         imsave(file_name, im, cmap = cm.gray)
 
     # plot a random sample
     fig, ax = plt.subplots(10, 10)
@@ -60,54 +60,44 @@ def part2(x, W, b):
 
 
 # Part 3
-def part3(x, W, y, b):
+def part3(x, W, b, y):
     """
     Compute the network loss function's gradient
     """
-    return dlossdw(x, W, y, b)
+    return dlossdw(x, W, b, y)
 
 
 # Test Part3
 def test_part3():
-    #PART 3b) Verification Code
-    M = loadmat("mnist_all.mat")
+    data = loadmat("mnist_all.mat")
 
-    #For our images, n = 7084, and our result ranges from 0 through 9, therefore k = 10.
-    # W is (n * k)
-    # W = np.ones(shape = (N_NUM,K_NUM))
-    N_NUM = 28 * 28
-    K_NUM = 10
-    M_TRAIN = 60000
-    W = np.random.rand(N_NUM, K_NUM)
-    b = np.ones(shape = (K_NUM,1))
+    n = 28 * 28
+    k = 10
+    num = sum([data["train" + str(i)].shape[0] for i in range(10)])
 
-    x = np.zeros(shape = (N_NUM, M_TRAIN))
-    y = np.zeros(shape = (K_NUM, M_TRAIN))
+    x = np.zeros((n, num))
+    W = np.random.rand(n, k)
+    b = np.ones((k, 1))
+    y = np.zeros((k, num))
 
     count = 0
-    #Load our example
     for i in range(10):
-        currSet = M["train" + str(i)].T / 255.0
-        x[:, count: count + currSet.shape[1]] = currSet
-        y[i, count: count + currSet.shape[1]] = 1
-        count += currSet.shape[1]
+        data_set = data["train" + str(i)] / 255.0
+        x[:, count: count + data_set.shape[0]] = data_set.T
+        y[i, count: count + data_set.shape[0]] = 1
+        count += data_set.shape[0]
 
-    scale = 2 #scale to only include a subsection of the 60000 images
-    b = b[:, 0:M_TRAIN / scale]
-    x = x[:, 0:M_TRAIN / scale]
-    y = y[:, 0:M_TRAIN / scale]
-
-    h = 0.00001
     grad = dlossdw(x, W, b, y)
 
-    print "done calculating gradient"
+    h = 0.000001
+    # Taking points in the middle of the image
+    points = [i * 28 + j for i in range(13, 16) for j in range(13, 16)]
+    for point in points:
+        W_calc = W.copy()
+        W_calc[point, 0] = W[point, 0] + h
+        approx_grad = (loss(x, W_calc, b, y) - loss(x, W, b, y)) / h
 
-    for i in range(10):
-        W_perturbed = W.copy()
-        W_perturbed[350 + i,0] = W[350 + i, 0] + h
-        approx_grad = (loss(x,W_perturbed,b,y) - loss(x,W,b,y))/h
-
-        print "grad: %.3f approximate grad: %.3f" %(grad[350 + i, 0], approx_grad)
+        print "gradient for [{}]: {:.3f}. Difference: {:.3f}".format(point, grad[point, 0], approx_grad)
 
     return
 
